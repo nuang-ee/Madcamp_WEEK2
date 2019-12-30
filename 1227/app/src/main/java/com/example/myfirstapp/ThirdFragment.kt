cd29e6d226adaea393c3ada71f2ff9b2e696dde8
Nuang_ee <random0411@kaist.ac.kr>
2019-12-30 16:59:21 +0900

    Update ThirdFragment.kt

--- 1227/app/src/main/java/com/example/myfirstapp/ThirdFragment.kt
+++ 1227/app/src/main/java/com/example/myfirstapp/ThirdFragment.kt
@@ -6,25 +6,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import android.content.Context
 import android.location.Address
 import android.location.Geocoder
+import android.os.AsyncTask
+import android.os.Build
 import android.util.Log
-import android.widget.Button
-import android.widget.TextView
+import android.widget.*
+import androidx.annotation.RequiresApi
 import com.example.myfirstapp.helper.GpsTracker
+import org.json.JSONObject
 import java.io.IOException
 import java.lang.ClassCastException
+import java.net.HttpURLConnection
+import java.net.URL
+import java.text.SimpleDateFormat
 import java.util.*
 
 
 class ThirdFragment : Fragment() {
+    val weatherAPI: String = "e2054e5fd85d82fb049fe2c791bfe058"
+    val fineDustAPI: String = "uo81tqCqaqN2cI45bKhC8%2BFOaEg6hvoiCxLRIDx1Ks4vPqUmHUHovDuiJJanHpbYEUvnCt2U4BdcoKgsUswjkQ%3D%3D"
+    var latitude: Double = 0.0
+    var longitude: Double = 0.0
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
         val view = inflater.inflate(R.layout.fragment_third, container, false)
 
         val textviewAddress = view.findViewById(R.id.location) as TextView
@@ -32,8 +43,8 @@ class ThirdFragment : Fragment() {
         ShowLocationButton.setOnClickListener(View.OnClickListener {
             val gpsTracker = GpsTracker(context)
 
-            val latitude = gpsTracker.latitude
-            val longitude = gpsTracker.longitude
+            latitude = gpsTracker.latitude
+            longitude = gpsTracker.longitude
 
             val address = getCurrentAddress(latitude, longitude)
             textviewAddress.text = address
@@ -43,14 +54,6 @@ class ThirdFragment : Fragment() {
 
             val cityName = addressParsed[1]
             val citySubName = addressParsed[2]
-
-            /*
-            Toast.makeText(
-                context,
-                "현재위치 \n위도 $latitude\n경도 $longitude",
-                Toast.LENGTH_LONG
-            ).show()
-            */
         })
         return view
     }
@@ -82,6 +85,78 @@ class ThirdFragment : Fragment() {
         return address.getAddressLine(0).toString() + "\n"
     }
 
+    @RequiresApi(Build.VERSION_CODES.CUPCAKE)
+    inner class weatherTask() : AsyncTask<String, Void, String>() {
+        override fun onPreExecute() {
+            super.onPreExecute()
+            /* Showing the ProgressBar, Making the main design GONE */
+            view?.findViewById<ProgressBar>(R.id.loader)?.visibility = View.VISIBLE
+            view?.findViewById<RelativeLayout>(R.id.mainContainer)?.visibility = View.GONE
+            view?.findViewById<TextView>(R.id.errorText)?.visibility = View.GONE
+        }
+
+        override fun doInBackground(vararg params: String?): String? {
+            var response:String?
+            try{
+                response = URL("https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&units=metric&appid=$weatherAPI").readText(
+                    Charsets.UTF_8
+                )
+            }catch (e: Exception){
+                response = null
+            }
+            return response
+        }
+
+        @RequiresApi(Build.VERSION_CODES.CUPCAKE)
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            try {
+                /* Extracting JSON returns from the API */
+                val jsonObj = JSONObject(result)
+                val main = jsonObj.getJSONObject("main")
+                val sys = jsonObj.getJSONObject("sys")
+                val wind = jsonObj.getJSONObject("wind")
+                val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
+
+                val updatedAt:Long = jsonObj.getLong("dt")
+                val updatedAtText = "Updated at: "+ SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(Date(updatedAt*1000))
+                val temp = main.getString("temp")+"°C"
+                val tempMin = "Min Temp: " + main.getString("temp_min")+"°C"
+                val tempMax = "Max Temp: " + main.getString("temp_max")+"°C"
+                val pressure = main.getString("pressure")
+                val humidity = main.getString("humidity")
+
+                val sunrise:Long = sys.getLong("sunrise")
+                val sunset:Long = sys.getLong("sunset")
+                val windSpeed = wind.getString("speed")
+                val weatherDescription = weather.getString("description")
+
+                val address = jsonObj.getString("name")+", "+sys.getString("country")
+
+                /* Populating extracted data into our views */
+                view?.findViewById<TextView>(R.id.address)?.text = address
+                view?.findViewById<TextView>(R.id.updated_at)?.text =  updatedAtText
+                view?.findViewById<TextView>(R.id.status)?.text = weatherDescription.capitalize()
+                view?.findViewById<TextView>(R.id.temp)?.text = temp
+                view?.findViewById<TextView>(R.id.temp_min)?.text = tempMin
+                view?.findViewById<TextView>(R.id.temp_max)?.text = tempMax
+                view?.findViewById<TextView>(R.id.sunrise)?.text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunrise*1000))
+                view?.findViewById<TextView>(R.id.sunset)?.text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunset*1000))
+                view?.findViewById<TextView>(R.id.wind)?.text = windSpeed
+                view?.findViewById<TextView>(R.id.pressure)?.text = pressure
+                view?.findViewById<TextView>(R.id.humidity)?.text = humidity
+
+                /* Views populated, Hiding the loader, Showing the main design */
+                view?.findViewById<ProgressBar>(R.id.loader)?.visibility = View.GONE
+                view?.findViewById<RelativeLayout>(R.id.mainContainer)?.visibility = View.VISIBLE
+
+            } catch (e: Exception) {
+                view?.findViewById<ProgressBar>(R.id.loader)?.visibility = View.GONE
+                view?.findViewById<TextView>(R.id.errorText)?.visibility = View.VISIBLE
+            }
+
+        }
+    }
     /*
     override fun onAttach(context: Context?) {
         super.onAttach(context)
