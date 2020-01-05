b5500fce391fb999927fc92618d8844ca8ab85df
Min-su Kim <Kim-mins@naver.com>
2020-01-05 09:19:02 +0000

    user check api done

--- server/routes/user/user.js
+++ server/routes/user/user.js
@@ -26,6 +26,22 @@ exports.addUser = (req, res) => {
         }
     })
 };
+// check if user is in database
+exports.checkUser = (req, res) => {
+    const { uid } = req.body;
+    userModel.find({ uid }, (err, [user]) => {  // [user], since find() returns list
+        if (err) {
+            console.error(err);
+            res.status(500).send(e);
+        } else {
+            if (user) {
+                res.json({ result: 1 }) // 1 if true
+            } else {
+                res.json({ result: 0 }) // 0 if there's no user in database
+            }
+        }
+    });
+}
 
 // after withdrawal
 exports.deleteUser = (req, res) => {
