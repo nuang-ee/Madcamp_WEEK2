7b6b1590bff2ce1d6edc9d2cdfc854ec3271e507
Min-su Kim <Kim-mins@naver.com>
2020-01-05 06:51:31 +0000

    updateContact almost done

--- server/routes/contact/contact.js
+++ server/routes/contact/contact.js
@@ -25,9 +25,6 @@ exports.addContact = (req, res) => {
             if (req.file == undefined) {
                 res.send('Error: No File Selected!');
             } else {
-                /**
-                 * Todo: user_id (facebook id)로 구별하기
-                 */
                 const { uid, name, phoneNumber, email, localCached } = req.body;
                 // find user
                 userModel.find({ uid }, (err, [user]) => {  // [user], since find() returns list
@@ -66,33 +63,50 @@ exports.addContact = (req, res) => {
 
 exports.updateContact = (req, res) => {
 
-    const { _id } = req.params
-    const { name, phoneNumber, email, thumbnail, localCached } = req.body;
+    // uid is user id, _id is id of contact to be updated
+    const { uid, _id, name, phoneNumber, email, localCached } = req.body;
+    console.log(req);
 
-    contactModel.findById(_id, (err, contact) => {
-        // database error
+    upload(req, res, (err) => {
         if (err) {
-            return res.status(500).json({ error: 'database failure' });
-        }
-        // no contact in database
-        if (!contact) {
-            return res.status(404).json({ error: 'contact not found' });
-        }
-
-        if (name) contact.name = name
-        if (phoneNumber) contact.phoneNumber = phoneNumber
-        if (email) contact.email = email
-        if (thumbnail) contact.thumbnail = thumbnail
-        if (localCached) contact.localCached = localCached
-
-        contact.save((err) => {
-            // update fail
-            if (err) {
-                res.status(500).json({ error: 'failed to update' });
+            console.log(err);
+        } else {
+            if (req.file == undefined) {
+                res.send('Error: No File Selected!');
             } else {
-                res.json({ message: 'contact updated' });
+                // find user in user collection
+                userModel.find({ uid }, (err, [user]) => {  // [user], since find() returns list
+                    if (err) {
+                        console.error(err);
+                        res.status(500).send(e);
+                    } else {
+                        contactModel.findById(_id, (err, contact) => {
+                            if (err) {
+                                res.statue(500).json({ error: 'database failure' })
+                            }
+                            if (!contact) {
+                                res.status(404).json({ error: 'contact not found' })
+                            }
+                            if (name) contact.name = name
+                            if (phoneNumber) contact.phoneNumber = phoneNumber
+                            if (email) contact.email = email
+                            if (req.file.filename) contact.thumbnail = req.file.filename;
+                            if (localCached) contact.localCached = localCached
+
+                            user.contact.push(contact);
+                            user.save((err) => {
+                                if (err) {
+                                    console.error(err);
+                                    res.json({ result: 0 });
+                                } else {
+                                    res.json({ result: 1 });
+                                }
+                            })
+                        })
+                    }
+                });
             }
-        })
+        }
     })
 }
 
--- server/routes/contact/index.js
+++ server/routes/contact/index.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
 })
 router.post('/get', contact.getContact);
 router.post('/add', contact.addContact);
-router.post('/update', contact.updateContact);
-router.post('/delete', contact.deleteContact);
+router.put('/update', contact.updateContact);
+router.delete('/delete', contact.deleteContact);
 
 module.exports = router;
