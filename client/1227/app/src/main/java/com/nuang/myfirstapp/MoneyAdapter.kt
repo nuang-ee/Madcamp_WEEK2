efff1364dfd4207399020280b3d66eaf57df6b67
Minsu <kim-mins@naver.com>
2020-01-07 19:53:01 +0900

    third tab front done

---
+++ client/1227/app/src/main/java/com/nuang/myfirstapp/MoneyAdapter.kt
@@ -0,0 +1,62 @@
+package com.nuang.myfirstapp
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
+
+
+class MoneyAdapter(val context: Context, val itemList: ArrayList<ClaimItem>): RecyclerView.Adapter<MoneyAdapter.Holder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = LayoutInflater.from(context).inflate(R.layout.claim_item, parent, false)
+        return Holder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bind(itemList[position])
+    }
+
+    inner class Holder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val name = itemView.findViewById<TextView>(R.id.claim_item_name)
+        val amount = itemView. findViewById<TextView>(R.id.claim_item_amount)
+
+        fun bind(item: ClaimItem) {
+            name?.text = item.name
+            amount?.text = item.amount.toString()
+
+            itemView.setOnClickListener {
+                val builder = AlertDialog.Builder(context)
+                val dialogView = LayoutInflater.from(context).inflate(R.layout.claimer_item_dialog, null)
+
+                val dialogName = dialogView.findViewById<TextView>(R.id.claimer_dialog_name_t)
+                val dialogAmount = dialogView.findViewById<TextView>(R.id.claimer_dialog_amount_t)
+                val dialogFrom = dialogView.findViewById<TextView>(R.id.claimer_dialog_from_t)
+                val dialogDate = dialogView.findViewById<TextView>(R.id.claimer_dialog_date_t)
+                val dialogAccount = dialogView.findViewById<TextView>(R.id.claimer_dialog_account_t)
+
+                dialogName?.text = item.name
+                dialogAmount?.text = item.amount.toString()
+                dialogFrom?.text = item.claimId
+                dialogDate?.text = item.date
+                dialogAccount?.text = item.account
+
+                builder.setView(dialogView)
+                    .setPositiveButton("확인") {
+                            dialogInterface, i ->
+
+                    }
+                    .show()
+            }
+        }
+    }
+}
